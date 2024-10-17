34de62cdc886713d6886173283e01a60f22b4d12
yazz <zubairq@gmail.com>
2024-10-17 18:08:39 +0200

    copy prod DB to local machine

--- getproddb.sh
+++ getproddb.sh
@@ -1,2 +1,9 @@
 ssh postgres@172.105.151.74 'pg_dump -U postgres tradingdb -F p -f tradingdb_backup.sql'
 scp root@172.105.151.74:/var/lib/pgsql/tradingdb_backup.sql .
+/usr/local/Cellar/postgresql@17/17.0_1/bin/psql -U postgres -c "drop database tradingdb"
+echo "Sleeping for 10 seconds..."
+sleep 2
+/usr/local/Cellar/postgresql@17/17.0_1/bin/psql -U postgres -c "CREATE DATABASE trading_db;" 
+echo "Sleeping for 10 seconds..."
+sleep 2
+/usr/local/Cellar/postgresql@17/17.0_1/bin/psql -U postgres -c tradingdb -f ./tradingdb_backup.sql
