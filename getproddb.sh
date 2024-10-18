3ad8ee771634b289f3b7559876c1cbb8612560e6
yazz <zubairq@gmail.com>
2024-10-18 06:48:58 +0200

    fix bug in db backup

--- getproddb.sh
+++ getproddb.sh
@@ -3,7 +3,7 @@ scp root@172.105.151.74:/var/lib/pgsql/tradingdb_backup.sql .
 /usr/local/Cellar/postgresql@17/17.0_1/bin/psql -U postgres -c "drop database tradingdb"
 echo "Sleeping for 10 seconds..."
 sleep 2
-/usr/local/Cellar/postgresql@17/17.0_1/bin/psql -U postgres -c "CREATE DATABASE trading_db;" 
+/usr/local/Cellar/postgresql@17/17.0_1/bin/psql -U postgres -c "CREATE DATABASE tradingdb;" 
 echo "Sleeping for 10 seconds..."
 sleep 2
 /usr/local/Cellar/postgresql@17/17.0_1/bin/psql -U postgres -c tradingdb -f ./tradingdb_backup.sql
