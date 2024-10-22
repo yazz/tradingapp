8a01d75d4c2fbbf8c21cddbdb3b481c40a056a6a
Zubair Quraishi <zubairq@gmail.com>
2024-10-22 15:39:17 +0200

    make small boxes in the UI

--- tui.js
+++ tui.js
@@ -9,11 +9,11 @@ screen.title = 'my window title';
 
 // Create a box perfectly centered horizontally and vertically.
 var box = blessed.box({
-    top: 'center',
-    left: 'center',
+    top: 'top',
+    left: 'left',
     width: '50%',
     height: '50%',
-    content: 'Hello {bold}world{/bold}!',
+    content: "Zubair's {bold}Trading system{/bold}!",
     tags: true,
     border: {
         type: 'line'
@@ -29,35 +29,45 @@ var box = blessed.box({
         }
     }
 });
-
-// Append our box to the screen.
 screen.append(box);
-
-// Add a png icon to the box
-var icon = blessed.image({
-    parent: box,
-    top: 0,
-    left: 0,
-    type: 'overlay',
-    width: 'shrink',
-    height: 'shrink',
-    file: __dirname + '/my-program-icon.png',
-    search: false
-});
-
-// If our box is clicked, change the content.
 box.on('click', function(data) {
-    box.setContent('{center}Some different {red-fg}content{/red-fg}.{/center}');
+    box.setContent('Started');
+    screen.render();
+});
+box.key('enter', function(ch, key) {
+    box.setContent('Started');
     screen.render();
 });
 
-// If box is focused, handle `enter`/`return` and give us some more content.
-box.key('enter', function(ch, key) {
-    box.setContent('{right}Even different {black-fg}content{/black-fg}.{/right}\n');
-    box.setLine(1, 'bar');
-    box.insertLine(1, 'foo');
-    screen.render();
+
+
+
+// Create a box perfectly centered horizontally and vertically.
+var box2 = blessed.box({
+    top: 'bottom',
+    right: 'right',
+    width: '50%',
+    height: '50%',
+    content: "Zubair's {bold}Trading system{/bold}!",
+    tags: true,
+    border: {
+        type: 'line'
+    },
+    style: {
+        fg: 'white',
+        bg: 'magenta',
+        border: {
+            fg: '#f0f0f0'
+        },
+        hover: {
+            bg: 'green'
+        }
+    }
 });
+screen.append(box2);
+
+
+
 
 // Quit on Escape, q, or Control-C.
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
