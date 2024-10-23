7905669aa26340eb031efeb1f073b63a235a5e66
yazz <zubairq@gmail.com>
2024-10-23 16:54:57 +0200

    Move uuidv4( ) to a lib

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 //--------------------
 
 
-
+let tr = require('./helpers.js')
 const fs                = require('fs');
 const { Client }        = require('pg');
 const yahooFinance      = require('yahoo-finance2').default;
@@ -19,12 +19,7 @@ let prices
 let client              = null
 
 
-function              uuidv4      (  ) {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-}
+
 async function getHistoricalStockPrices(symbol, startDate, endDate) {
     try {
         // Fetch historical prices
@@ -66,7 +61,7 @@ async function main() {
                         if (exists.rowCount == 0) {
                             console.error('3');
                             await client.query("insert into source_yahoo_finance_daily_stock_data (id,symbol,stock_date,stock_adj_close,stock_volume,stock_open,stock_close,stock_high,stock_low) values ($1,$2,$3,$4,$5,$6,$7,$8,$9)",
-                            [uuidv4(),stock,price.date, price.adjClose, price.volume, price.open, price.close,  price.high, price.low])
+                            [tr.helpers.uuidv4(),stock,price.date, price.adjClose, price.volume, price.open, price.close,  price.high, price.low])
                             console.error('4');
                         }
                     
