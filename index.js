c4182a21f4ca13bbc254665dc39e72ec7c42b3cb
yazz <zubairq@gmail.com>
2024-11-12 17:46:55 +0100

    top pane created

--- index.js
+++ index.js
@@ -22,157 +22,110 @@ let app = {
             app.vars.screen.title = 'ZAlgoHedgeFund';
         },
         createTopPane: function() {
+            // Create a box perfectly centered horizontally and vertically.
+            app.vars.box = blessed.box({
+                top: 'top',
+                left: 'left',
+                width: '100%',
+                height: '20%',
+                content: "{bold}ZAlgoHedgeFund{/bold}!",
+                tags: true,
+                border: {
+                    type: 'line'
+                },
+                style: {
+                    fg: 'white',
+                    bg: 'magenta',
+                    border: {
+                        fg: '#f0f0f0'
+                    },
+                    hover: {
+                        bg: 'green'
+                    }
+                }
+            });
+            app.vars.screen.append(app.vars.box);
+            app.vars.box.on('click', function (data) {
+                box.setContent(tr.helpers.getDemoText());
+                app.vars.screen.render();
+            });
+            app.vars.box.key('enter', function (ch, key) {
+                app.vars.box.setContent(tr.helpers.getDemoText());
+                app.vars.screen.render();
+            });
 
+            // Create a button
+            const button = blessed.button({
+                parent: app.vars.box,  // Attach button to the box
+                mouse: true,
+                keys: true,
+                shrink: true,
+                padding: {
+                    left: 2,
+                    right: 2,
+                },
+                left: 0,
+                bottom: 0,  // Position within the box
+                name: 'submit',
+                content: 'Click me!',
+                style: {
+                    bg: 'blue',
+                    fg: 'white',
+                    focus: {
+                        bg: 'green',
+                    },
+                    hover: {
+                        bg: 'green',
+                    },
+                },
+            });
+
+            // Button click event
+            button.on('click', async function () {
+                app.vars.box.setContent('Button was clicked!');
+                //table.setData(tr.helpers.convertToArrayOfArrays(processes))
+                app.vars.screen.render()
+                app.vars.screen.render(); // Re-render the screen to show changes
+            });
+
+            // Create a button
+            const button2 = blessed.button({
+                parent: app.vars.box,  // Attach button to the box
+                mouse: true,
+                keys: true,
+                shrink: true,
+                padding: {
+                    left: 2,
+                    right: 2,
+                },
+                left: 15,
+                bottom: 0,  // Position within the box
+                name: 'submit',
+                content: '2ns!',
+                style: {
+                    bg: 'blue',
+                    fg: 'white',
+                    focus: {
+                        bg: 'green',
+                    },
+                    hover: {
+                        bg: 'green',
+                    },
+                },
+            });
+            button2.on('click', async function () {
+                console.log("node ./get_prices.js")
+                tr.helpers.execCommand("node ./get_prices.js")
+            })
         }
     },
     main:               async function  (  ) {
         let client = await tr.helpers.connectDb(config)
 
         app.screen.createScreen()
-
-        // Create a box perfectly centered horizontally and vertically.
-        app.vars.box = blessed.box({
-            top: 'top',
-            left: 'left',
-            width: '50%',
-            height: '50%',
-            content: "{bold}ZAlgoHedgeFund{/bold}!",
-            tags: true,
-            border: {
-                type: 'line'
-            },
-            style: {
-                fg: 'white',
-                bg: 'magenta',
-                border: {
-                    fg: '#f0f0f0'
-                },
-                hover: {
-                    bg: 'green'
-                }
-            }
-        });
-        app.vars.screen.append(app.vars.box);
-        app.vars.box.on('click', function (data) {
-            box.setContent(tr.helpers.getDemoText());
-            app.vars.screen.render();
-        });
-        app.vars.box.key('enter', function (ch, key) {
-            app.vars.box.setContent(tr.helpers.getDemoText());
-            app.vars.screen.render();
-        });
+        app.screen.createTopPane()
 
 
-// Create a button
-        const button = blessed.button({
-            parent: app.vars.box,  // Attach button to the box
-            mouse: true,
-            keys: true,
-            shrink: true,
-            padding: {
-                left: 2,
-                right: 2,
-            },
-            left: 0,
-            bottom: 0,  // Position within the box
-            name: 'submit',
-            content: 'Click me!',
-            style: {
-                bg: 'blue',
-                fg: 'white',
-                focus: {
-                    bg: 'green',
-                },
-                hover: {
-                    bg: 'green',
-                },
-            },
-        });
-
-// Button click event
-        button.on('click', async function () {
-            app.vars.box.setContent('Button was clicked!');
-            //table.setData(tr.helpers.convertToArrayOfArrays(processes))
-            app.vars.screen.render()
-            app.vars.screen.render(); // Re-render the screen to show changes
-        });
-
-
-// Create a button
-        const button2 = blessed.button({
-            parent: app.vars.box,  // Attach button to the box
-            mouse: true,
-            keys: true,
-            shrink: true,
-            padding: {
-                left: 2,
-                right: 2,
-            },
-            left: 15,
-            bottom: 0,  // Position within the box
-            name: 'submit',
-            content: '2ns!',
-            style: {
-                bg: 'blue',
-                fg: 'white',
-                focus: {
-                    bg: 'green',
-                },
-                hover: {
-                    bg: 'green',
-                },
-            },
-        });
-        button2.on('click', async function () {
-            console.log("node ./get_prices.js")
-            tr.helpers.execCommand("node ./get_prices.js")
-        })
-
-// Create a box perfectly centered horizontally and vertically.
-        var positionsWindow = blessed.box({
-            top: 'bottom',
-            right: '0',
-            width: '50%',
-            height: '50%',
-            content: "Positions",
-            tags: true,
-            border: {
-                type: 'line'
-            },
-            style: {
-                fg: 'white',
-                bg: 'magenta',
-                border: {
-                    fg: '#f0f0f0'
-                },
-                hover: {
-                    bg: 'green'
-                }
-            }
-        });
-        app.vars.screen.append(positionsWindow);
-// Create a listtable widget
-
-
-
-        let positions = await client.query("select  *  from  trading_positions",[])
-
-        const positionsTable = blessed.listtable({
-            parent: positionsWindow,
-            top: 'center',
-            left: 'center',
-            width: '80%',
-            height: '50%',
-            border: {type: 'line'},
-            align: 'center', // Align text in the cells
-            style: {
-                header: {fg: 'blue', bold: true},
-                cell: {fg: 'white', selected: {bg: 'blue'}},
-            },
-            keys: true, // Allows navigation using arrow keys
-            mouse: true, // Allows interaction using the mouse
-            data: tr.helpers.convertToArrayOfArrays(positions.rows),
-        });
 
 
 // Create a box perfectly centered horizontally and vertically.
