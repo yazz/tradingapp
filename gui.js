5073e1c94b0e13d1306a64cbbafe833dd330b182
yazz <zubairq@gmail.com>
2024-11-14 13:25:50 +0100

    Created a basic input form with w2ui

---
+++ gui.js
@@ -0,0 +1,4 @@
+import { w2grid } from 'https://rawgit.com/vitmalina/w2ui/master/dist/w2ui.es6.min.js'
+import { w2utils } from 'https://rawgit.com/vitmalina/w2ui/master/dist/w2ui.es6.min.js'
+console.log ("Version of w2ui is:",w2utils.version)
+
